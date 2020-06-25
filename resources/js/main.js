238e24162f633c87498b0bd03523a236ebc72a41
rrrhhhhhhhhh <rob.halverson@gmail.com>
2020-06-25 11:40:10 -0400

    Create main.js

---
+++ resources/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+
+    $( document ).ready(function() {
+
+
+
+        var windowWidth = $(window).width(); //retrieve current window width
+        var windowHeight = $(window).height(); //retrieve current window height
+
+        var gridCount = 5;
+        var topToBottomCount = 3;
+
+        var gridWidth = windowWidth / gridCount;
+
+
+    });
+
+
+})(jQuery);
